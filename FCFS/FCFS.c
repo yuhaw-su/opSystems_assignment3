a15af62f123eeb6764eb38783582f9bf8cf2cdb4
Yuhaw Su <yuhawsu@gmail.com>
2016-10-18 23:01:31 -0500

    and we off

---
+++ FCFS/FCFS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+
+}
